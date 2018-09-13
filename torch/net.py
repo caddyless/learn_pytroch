7131137467380490b539f529198b28d6b0f6089e
lijin <874900712@qq.com>
2018-09-13 12:58:31 +0800

    common commit

--- torch/net.py
+++ torch/net.py
@@ -3,13 +3,15 @@ from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         # 1 input image channel, 6 output channels, 5*5 square convolution
         # kernel
 
-        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
         # an affine operation: y = Wx + b
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
@@ -28,16 +30,18 @@ class Net(nn.Module):
         return x
 
     def num_flat_features(self, x):
-        size = x.size()[1:] # all dimensions except the batch dimension
+        size = x.size()[1:]  # all dimensions except the batch dimension
         num_features = 1
         for s in size:
             num_features *= s
         return num_features
+
+
 net = Net()
 params = list(net.parameters())
 input = Variable(torch.randn(1, 1, 32, 32))
 out = net(input)
-target = Variable(torch.ones(1,10).float())  # a dummy target, for example
+target = Variable(torch.ones(1, 10).float())  # a dummy target, for example
 criterion = nn.MSELoss()
 
 loss = criterion(out, target)
