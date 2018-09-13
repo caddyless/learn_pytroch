cad09fc751d315411a3edd4cf34a23ffb73dd08e
lijin <874900712@qq.com>
2018-09-13 13:02:31 +0800

    common commit

--- torch/get_image.py
+++ torch/get_image.py
@@ -1,7 +1,7 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 
 classes = ('plane', 'car', 'bird', 'cat',
@@ -11,21 +11,21 @@ classes = ('plane', 'car', 'bird', 'cat',
 def imshow(img):
     img = img / 2 + 0.5     # unnormalize
     npimg = img.numpy()
+    plt.figure('image')
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
-if __name__ == '__main__':
+def download_img():
     transform = transforms.Compose(
         [transforms.ToTensor(),
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                             download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                              shuffle=True, num_workers=2)
-
     testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                              shuffle=True, num_workers=2)
     testloader = torch.utils.data.DataLoader(testset, batch_size=4,
                                              shuffle=False, num_workers=2)
     # get some random training images
@@ -36,3 +36,10 @@ if __name__ == '__main__':
     imshow(torchvision.utils.make_grid(images))
     # print labels
     print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+    return trainloader,testloader
+
+
+if __name__ == '__main__':
+    #download cifar10
+    trainset,testset=download_img()
