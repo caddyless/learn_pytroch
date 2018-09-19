ea67f5c259a574a4e232c6cc298e5a7dab13550c
Jin Li <834908611@qq.com>
2018-09-19 15:48:36 +0800

    common commit

--- torch/torch_test.py
+++ torch/torch_test.py
@@ -1,11 +1,28 @@
-from __future__ import  print_function
-import torch
-from torch.autograd import Variable
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
 
-def fn_add(x,y):
-    x=x+y
+def get_img():
+    I=cv2.imread('beauty.jpg')
+    I=cv2.cvtColor(I,cv2.COLOR_BGR2GRAY)
+    I=np.array(I)
+    return I
 
-x=5
-y=10
-fn_add(x,y)
-print(x)
+def show_imgs(img_list):
+    plt.figure('compare')
+    num=0
+    for img in img_list:
+        plt.subplot(231)
+        plt.imshow(img, cmap='gray')
+        plt.show()
+
+def svd(array):
+    U,sigma,Vt=np.linalg.svd(array,full_matrices=False)
+    print(sigma)
+
+
+if __name__ == '__main__':
+    img=get_img()
+    svd(img)
+
+
