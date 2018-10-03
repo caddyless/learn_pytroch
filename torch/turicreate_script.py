4b86811d489797c40741d0608631d2d524d65245
caddyless <834908611@qq.com>
2018-10-03 13:10:38 +0800

    changes to turicreate_script

--- torch/turicreate_script.py
+++ torch/turicreate_script.py
@@ -6,22 +6,27 @@ turicreate.config.set_num_gpus(-1)
 
 def create_mod(dir):
     path=os.path.join(source_dir,dir)
-    if os.path.isfile(path+'/caltech-101.sframe'):
+    if os.path.isfile(path+'/data.sframe'):
+        print('reference_data is existed')
+    else:
+        # Load images from the downloaded data
+        reference_data = tc.image_analysis.load_images(path)
+        reference_data = reference_data.add_row_number()
+    if os.path.isfile(path+'/savedmodel.model'):
         print('mod is existed')
-        return
-    # Load images from the downloaded data
-    reference_data = tc.image_analysis.load_images(path)
-    reference_data = reference_data.add_row_number()
-
-    # Save the SFrame for future use
-    reference_data.save(path+'/caltech-101.sframe')
-    model = tc.image_similarity.create(reference_data)
-    model.save(path+'/savedmodel.model')
+    else:
+        # Save the SFrame for future use
+        reference_data.save(path + '/data.sframe')
+        model = tc.image_similarity.create(reference_data)
+        model.save(path + '/savedmodel.model')
     return
 
 def compare_img(folder):
     path=os.path.join(source_dir,folder)
     loaded_model = turicreate.load_model(path+'/savedmodel.model')
+    reference_data=turicreate.load_sframe(path+'/data.sframe')
+    query_results = loaded_model.query(reference_data[0:10], k=10)
+    query_results.head()
 
 
 
