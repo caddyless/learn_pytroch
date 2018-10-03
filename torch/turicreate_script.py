9c39e9e32faef46177f1e76460c3a1123f8f7b94
caddyless <834908611@qq.com>
2018-10-03 13:51:18 +0800

    changes to turicreate_script

--- torch/turicreate_script.py
+++ torch/turicreate_script.py
@@ -1,37 +1,46 @@
 import turicreate as tc
 import os
 
-source_dir='./new_seg_images'
-turicreate.config.set_num_gpus(-1)
+source_dir = '/home/qualcomm/new_seg_images'
+origin_dir = '/home/qualcomm/101_ObjectCategories'
 
-def create_mod(dir):
-    path=os.path.join(source_dir,dir)
-    if os.path.isfile(path+'/data.sframe'):
+
+def create_mod(path):
+    if os.path.isfile(path + '/data.sframe'):
         print('reference_data is existed')
+        reference_data = turicreate.load_sframe(path + '/data.sframe')
     else:
         # Load images from the downloaded data
         reference_data = tc.image_analysis.load_images(path)
         reference_data = reference_data.add_row_number()
-    if os.path.isfile(path+'/savedmodel.model'):
+    if os.path.isfile(path + '/savedmodel.model'):
         print('mod is existed')
+        model = turicreate.load_model(path + '/savedmodel.model')
     else:
         # Save the SFrame for future use
         reference_data.save(path + '/data.sframe')
         model = tc.image_similarity.create(reference_data)
         model.save(path + '/savedmodel.model')
-    return
+    return reference_data, model
 
-def compare_img(folder):
-    path=os.path.join(source_dir,folder)
-    loaded_model = turicreate.load_model(path+'/savedmodel.model')
-    reference_data=turicreate.load_sframe(path+'/data.sframe')
-    query_results = loaded_model.query(reference_data[0:10], k=10)
-    query_results.head()
 
+def compare_img(origin_results,current_results):
+    o=origin_results
+    c=current_results
+    assert len(o)==len(c) ,'len(origin_results) are not equal to len(current_results)'
+
+
+def query(path,start=0,end=10,k=10):
+    reference_data,model=create_mod(path)
+    query_results = model.query(reference_data[0:10], k=10)
+    return query_results
 
 
 if __name__ == '__main__':
-    folders=os.listdir(source_dir)
+    turicreate.config.set_num_gpus(-1)
+    origin_results=query(origin_dir)
+    folders = os.listdir(source_dir)
     for folder in folders:
-        data=create_mod(folder)
-        compare_img(folder)
+        path = os.path.join(source_dir, folder)
+        current_results=query(path)
+        compare_img(origin_results,current_results)
