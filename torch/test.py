5c7693efecae3619643bc46ab388e70fee66b724
caddyless <834908611@qq.com>
2018-10-11 17:16:52 +0800

    make the change function work

--- torch/test.py
+++ torch/test.py
@@ -1,7 +1,28 @@
 import numpy as np
 import ITQ
 
-a=[[1,2],[2,3],[4,5]]
-a=np.array(a)
-a=a.reshape(2,3)
-print(a)
+Y = np.arange(24)
+Y = Y.reshape(4, 6)
+A = Y.dot(Y.T)
+eivalues, eivector = np.linalg.eig(A)
+
+sorted_indices = np.argsort(-eivalues)
+eivalues = np.array([eivalues[item] for item in sorted_indices])
+eivector = np.array([eivector[:, item] for item in sorted_indices])
+index = 0
+for i in range(len(eivalues)):
+    ratio = eivalues[:(i+1)].sum()/eivalues.sum()
+    if ratio > 0.95:
+        index = i+1
+        break
+Ud_pie = eivector[:index]
+print(Ud_pie)
+test_eigen = eivector[0].reshape((4, 1))
+print(test_eigen)
+print(index)
+print(eivector.shape)
+print(test_eigen.shape)
+print(A.dot(test_eigen))
+print(eivalues[0]*test_eigen)
+print(A)
+print(test_eigen.dot(test_eigen.T)*eivalues[0])
